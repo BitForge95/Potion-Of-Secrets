5fef69a2538c5fefe1d050eca6581dd469680b9a
Charan Sai <guttulacharansai@gmail.com>
2025-08-07 00:55:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [secretInput, setSecretInput] = useState("");
+  const [randomSecret, setRandomSecret] = useState("");
+  const [message, setMessage] = useState("")
+
+
+  const fetchRandomSecret = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/secret");
+      if (res.data.data) {
+        setRandomSecret(res.data.data);
+      } else {
+        setRandomSecret("No secrets yet!");
+      }
+    } catch (error) {
+      console.error("Error fetching secret:", error);
+      setRandomSecret("Error fetching secret");
+    }
+  };
+
+  const addSecret = async (e) => {
+    e.preventDefault();
+    if (!secretInput) return;
+
+    try {
+      await axios.post("http://localhost:3000/api/secret", {
+        randomSecret: secretInput,
+      });
+      setSecretInput("");
+      setMessage("Your Secret is Safe within us. Like a Horcrux !")
+      fetchRandomSecret();
+    } catch (error) {
+      setMessage("Sorry Please try adding yor Secret Again");
+      console.error("Error adding secret:", error);
+    }
+  };
+ 
+  useEffect(() => {
+    fetchRandomSecret();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main-Container">
+      <h1>You Tell us a Secret, We'll Tell you One</h1>
+
+      <form onSubmit={addSecret} className="Form-Container">
+        <input
+          type="text"
+          value={secretInput}
+          onChange={(e) => setSecretInput(e.target.value)}
+          placeholder="Enter your secret"
+          className="Input-Box"
+          
+        />
+        <p className="sub-message">{message}</p>
+        
+        <button type="submit" className="Submit-Button">
+          Add Secret
+        </button>
+      </form>
+
+      <div>
+        <h2 className="Random-Secret-Heading">Random Secret:</h2>
+        <p className="Random-Secret">{randomSecret || "No secrets yet!"}</p>
+        <button onClick={fetchRandomSecret} className="Fetch-Secret-Button">
+          Show Another Secret
+        </button>
+      </div>
     </div>
   );
 }
